d12f54ed2e8f7dc701ab12b7008c0587e276463d
axton21 <axton_n@yahoo.com>
2015-05-14 00:12:09 -0400

    done

--- js/selectors.js
+++ js/selectors.js
@@ -7,4 +7,25 @@ $(document).ready(function(){
   checkboxInputSelector();
 });
 
-// declare your functions here...
+function h1Selector(){
+  return $("h1")
+};
+
+function liInOlSelector(){
+  return $("ol li")
+};
+
+function linkSelector(){
+  // an id of box4 and a class of box5.'
+  return $("#box4, #box5 a")
+};
+
+function imageSelector(){
+  // alt text equal to "cat sleeping"'
+  return $('img[alt=\"cat sleeping\"]')
+};
+
+function checkboxInputSelector(){
+  //should select all checkbox inputs and wrap them with a span that has the class of pinky using the wrap method
+  $( 'input[type=\"checkbox\"]' ).wrap( "<span class='pinky'></span>" );
+};
